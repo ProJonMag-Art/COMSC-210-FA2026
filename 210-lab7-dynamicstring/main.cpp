1321364bdc248d57a5a5c234a4ba6adc2c4716cd
Jonvianney Maglasang <ProJonMag@gmail.com>
2026-09-11 18:11:51 -0700

    lab7 Commit 2: Added comments, fixed displayArray intialization problems, attempted to create example object, but ran into issues

--- 210-lab7-dynamicstring/main.cpp
+++ 210-lab7-dynamicstring/main.cpp
@@ -1,17 +1,33 @@
 
+// COMSC-210 | Lab 7 | Jonvianney Maglasang
+
+// Started September 11, 2026 at 5:58
+// Finished September 11, 2026 at
+
 #include <iostream>
 #include <string>
 
 using namespace std;
 
+// Takes string pointer array of arrSize and
 void reverseArray(string*& strArr, int arrSize);
-void outputArray(string* strArr, int arrSize);
-void displayArray(string* strArr, int arrSize);
+
+// Takes string pointer array of arrSize
+// Loops up until the size and couts each string in the array
+void outputArrayContents(string* strArr, int arrSize);
+
+// Takes string pointer array of arrSize
+// Runs outputArrayContents() to cout contents then runs reverseArray()
+// After flipping, it runs outputArrayContents() one more time to cout the transformed array
+void displayArray(string*& strArr, int arrSize);
 
 const int arrSize = 5;
 
 int main()
 {
+    string* namesArr = new string[arrSize];
+    namesArr = { "Janet", "Jeffe", "Jin", "Joe", "Junio" };
+    displayArray(namesArr, arrSize);
 
     return 0;
 }
@@ -21,7 +37,7 @@ void reverseArray(string*& strArr, int arrSize)
 
 }
 
-void outputArray(string* strArr, int arrSize)
+void outputArrayContents(string* strArr, int arrSize)
 {
     for(int i = 0; i < arrSize; i++)
     {
@@ -33,9 +49,9 @@ void outputArray(string* strArr, int arrSize)
 void displayArray(string*& strArr, int arrSize)
 {
     cout << "Original Array: ";
-    outputArray(strArr, arrSize);
+    outputArrayContents(strArr, arrSize);
 
     cout << "Reversed Array: ";
     reverseArray(strArr, arrSize);
-    outputArray(strArr, arrSize);
+    outputArrayContents(strArr, arrSize);
 }
