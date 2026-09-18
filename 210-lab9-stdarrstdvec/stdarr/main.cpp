6e335c82e0d9f53bd0adbbeda03c8d5950bc0041
Jonvianney Maglasang <ProJonMag@gmail.com>
2026-09-17 17:55:52 -0700

    lab 9 Commit 3: Removed readData(), declared getAverage(), declared getDataCheckpoints(), declared outputData(). Started defining getAverage() Having trouble adding more std::array method implementations

--- 210-lab9-stdarrstdvec/stdarr/main.cpp
+++ 210-lab9-stdarrstdvec/stdarr/main.cpp
@@ -15,12 +15,14 @@ const int arrSize = 30;
 
 // Takes string filename as input to read data into an std::array through a loop going through its array size
 void readData(string fileName, array<float, 30>& dataArr);
-void writeData(string fileName, array<float, 30> dataArr);
+float getAverage(array<float, 30> dataArr);
+array<float, 3> getDataCheckpoints(array<float, 30> dataArr);
+void outputData(array<float, 30> dataArr);
 
 int main()
 {
     array<float, arrSize> gradeData;
-    gradeData.fill(0);
+    gradeData.fill(-1);
 
     readData("data.txt", gradeData);
 
@@ -40,15 +42,16 @@ void readData(string fileName, array<float, 30>& dataArr)
     readFile.close();
 }
 
-void readData(string fileName, array<float, 30>& dataArr)
+float getAverage(array<float, 30> dataArr)
 {
-    ofstream writeFile(fileName);
-    int readSize = dataArr.size();
+    int arrSize = dataArr.size();
+    float sum = 0;
 
-    for(int i = 0; i < readSize; i++)
+    for(int i = 0; i < arrSize; i++)
     {
-        writeFile << dataArr[i];
+        if(dataArr.at(i) != -1)
+        {
+            sum += dataArr.at(i);
+        }
     }
-
-    writeFile.close();
 }
