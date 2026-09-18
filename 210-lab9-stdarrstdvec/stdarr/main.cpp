afaa8767d7827e0aaf547c736316e0e63c7bf520
Jonvianney Maglasang <ProJonMag@gmail.com>
2026-09-17 17:42:53 -0700

    lab9 Commit 2: Finished writeData(), copied into readData() to edit.

--- 210-lab9-stdarrstdvec/stdarr/main.cpp
+++ 210-lab9-stdarrstdvec/stdarr/main.cpp
@@ -13,11 +13,42 @@ using namespace std;
 
 const int arrSize = 30;
 
-void readData(string filename);
-void writeData(string filename);
+// Takes string filename as input to read data into an std::array through a loop going through its array size
+void readData(string fileName, array<float, 30>& dataArr);
+void writeData(string fileName, array<float, 30> dataArr);
 
 int main()
 {
+    array<float, arrSize> gradeData;
+    gradeData.fill(0);
+
+    readData("data.txt", gradeData);
 
     return 0;
+}
+
+void readData(string fileName, array<float, 30>& dataArr)
+{
+    ifstream readFile(fileName);
+    int readSize = dataArr.size();
+
+    for(int i = 0; i < readSize; i++)
+    {
+        readFile >> dataArr[i];
+    }
+
+    readFile.close();
+}
+
+void readData(string fileName, array<float, 30>& dataArr)
+{
+    ofstream writeFile(fileName);
+    int readSize = dataArr.size();
+
+    for(int i = 0; i < readSize; i++)
+    {
+        writeFile << dataArr[i];
+    }
+
+    writeFile.close();
 }
