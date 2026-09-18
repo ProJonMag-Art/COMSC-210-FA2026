709a62aef6077ba1d8fb0869d17e15d0302d10ee
Jonvianney Maglasang <ProJonMag@gmail.com>
2026-09-17 17:30:10 -0700

    lab9 Commit 1: Added boiler plate, libraries, function declarations

---
+++ 210-lab9-stdarrstdvec/stdarr/main.cpp
@@ -0,0 +1,23 @@
+
+// COMSC-210 | Lab 7 | Jonvianney Maglasang
+
+// Started September 17, 2026 at 5:17pm
+// Finished September 17, 2026 at
+
+#include <iostream>
+#include <fstream>
+#include <array>
+#include <string>
+
+using namespace std;
+
+const int arrSize = 30;
+
+void readData(string filename);
+void writeData(string filename);
+
+int main()
+{
+
+    return 0;
+}
