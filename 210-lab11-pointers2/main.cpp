e9d1a6d4131c919bf7ec0589ec7bb8c7bd17879e
Jonvianney Maglasang <ProJonMag@gmail.com>
2026-09-20 12:45:25 -0700

    lab 11 Commit 4: Trying to output data, and might define print item data function or struct function

--- 210-lab11-pointers2/main.cpp
+++ 210-lab11-pointers2/main.cpp
@@ -5,10 +5,10 @@
 // Finished September 20, 2026 at 
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 
 using namespace std;
-
 
 // Stores
 struct Date
@@ -23,15 +23,22 @@ struct Date
         this->year = year;
     }
 
-    // Default Constructor makes items have 0 values, but will be updates in the future
+    // Default Constructor makes items have 0 values, but allows updates in the future
     Date()
     {
         this->month = 0;
         this->day = 0;
         this->year = 0;
     }
+
+    // Couts date in M/D/Y
+    void printDate()
+    {
+        cout << this->month << "/" << this->day << "/" << this->year;
+    }
 };
 
+// Struct Declarations
 // Stores the name of the item, when the item was first listed, it's price, and a dynamic array containing any the full record of prices (stores the date of new and old prices)
 struct Item
 {
@@ -65,7 +72,15 @@ struct Item
     }
 };
 
+// Global Variables
 const int arrSize = 10;
+
+// Function Declarations
+void printItemArr(Item item)
+{
+    item.date.printDate();
+    
+}
 
 int main()
 {
@@ -73,6 +88,9 @@ int main()
     for(int i = 0; i < arrSize; i++)
     {
         itemArr[i] = Item();
+
+        itemArr[i].date.printDate(); // Couts full date M/D/Y
+        cout << itemArr[i].name << " " << itemArr[i].price;
     }
 
     return 0;
